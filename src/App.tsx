b3d95606c6334bf673be870e33f7b927b2312f2a
gautamVaja <gautamiitmee@gmail.com>
2021-10-12 15:07:14 +0530

    footer add

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Contact from "./Pages/Contact";
 
 import { TransitionGroup } from "react-transition-group";
 import MyRoutes from "./Components/MyRoutes";
+import { AnimatePresence, motion } from "framer-motion";
 
 //todo add animations while routing and rendering
 
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { AppBar, Grid, Container, Toolbar, Typography } from "@mui/material";
 import { motion } from "framer-motion";
 
 export default function Footer() {
@@ -8,9 +8,55 @@ export default function Footer() {
       animate={{ translateY: [200, 0], opacity: [0.3, 1] }}
       transition={{ duration: 1.5, ease: "linear" }}
     >
-      <Grid container p={5} justifyContent="center" sx={{ fontSize: "3rem" }}>
-        This is my portfolio website
-      </Grid>
+      <AppBar position="static" color="inherit">
+        <Grid
+          container
+          justifyContent="space-evenly"
+          pt={3}
+          pb={6}
+          sx={{ borderTop: "2px black solid" }}
+        >
+          <Grid item>
+            <Typography variant="h6" color="initial">
+              Phone
+            </Typography>
+            <Typography variant="subtitle1" color="intial">
+              8779536074
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6" color="initial">
+              Email:
+            </Typography>
+            <Typography variant="subtitle1" color="intial">
+              gautamiitmee@gmail.com
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" color="inherit">
+              Follow me
+            </Typography>
+            <img
+              alt="LinkedIn"
+              src="https://static.wixstatic.com/media/6ea5b4a88f0b4f91945b40499aa0af00.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/6ea5b4a88f0b4f91945b40499aa0af00.webp"
+              style={{ width: "24px", height: "24px" }}
+            ></img>
+            <img
+              alt="Twitter"
+              src="https://static.wixstatic.com/media/c7d035ba85f6486680c2facedecdcf4d.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/c7d035ba85f6486680c2facedecdcf4d.webp"
+              style={{ width: "24px", height: "24px" }}
+            ></img>
+            <img
+              alt="Github"
+              src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
+              style={{ width: "24px", height: "24px" }}
+            ></img>
+          </Grid>
+          <Typography variant="body1" color="inherit">
+            © 2021 By Gautam Vaja
+          </Typography>
+        </Grid>
+      </AppBar>
     </motion.div>
   );
 }
